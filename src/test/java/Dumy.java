9f2f60337882e68f2e1d67dc81add550274f7bee
reahmedreda <reahmedreda@gmail.com>
2023-10-01 21:47:33 +0300

    dummy test

---
+++ src/test/java/Dumy.java
@@ -0,0 +1,16 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Dumy {
+
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,2);
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,1);
+    }
+
+}
